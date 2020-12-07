09775e9c9821be2a52ddd5c9344b34199df93950
Lucas-Raphael Mueller <lucas-raphael.mueller@dkfz-heidelberg.de>
2020-12-07 14:25:27 +0100

    Forgot return argument

--- pytorch_utils/plot/plot.py
+++ pytorch_utils/plot/plot.py
@@ -12,4 +12,4 @@ def tshow(t: torch.Tensor, autosqueeze: bool = True, *args, **kwargs):
     if t.dim() == 3:
         t = t.permute(1, 2, 0)
 
-    plt.imshow(t, *args, **kwargs)
+    return plt.imshow(t, *args, **kwargs)
