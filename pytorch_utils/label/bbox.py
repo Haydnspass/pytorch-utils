317734ff8eec96833c56767798fec72a462501a6
Lucas-Raphael Mueller <lucas-raphael.mueller@dkfz-heidelberg.de>
2021-03-24 15:35:18 +0100

    Add cxcywh

--- pytorch_utils/label/bbox.py
+++ pytorch_utils/label/bbox.py
@@ -1,17 +1,52 @@
 import torch
 
 
-def convert_bbox(box: torch.Tensor, mode_in, mode_out) -> torch.Tensor:
+def convert_bbox(box: torch.Tensor, mode_in: str, mode_out: str) -> torch.Tensor:
+    """
+    Convert bounding boxes in format N x 4 between xyxy and xywh formats.
+
+    Args:
+        box: input boxes
+        mode_in: xyxy or xywh
+        mode_out: xyxy or xywh
+
+    """
     if mode_in == mode_out:
         return box
 
+    # always convert to xyxy first and derive from that base
+    box_xyxy = _bbox_arbitrary_to_xyxy(box, mode=mode_in)
+    box_out = _bbox_xyxy_to_arbitrary(box_xyxy, mode=mode_out)
+
+    return box_out
+
+
+def _bbox_arbitrary_to_xyxy(box: torch.Tensor, mode: str) -> torch.Tensor:
+
+    if mode == 'xyxy':
+        return box
+
     box_out = box.clone()
-    if mode_in == 'xyxy' and mode_out == 'xywh':
-        box_out[:, 2:] = box[:, 2:] - box[:, :2]
-
-    elif mode_in == 'xywh' and mode_out == 'xyxy':
+    if mode == 'xywh':
         box_out[:, 2:] = box[:, :2] + box[:, 2:]
+    else:
+        raise NotImplementedError
 
+    return box_out
+
+def _bbox_xyxy_to_arbitrary(box: torch.Tensor, mode: str) -> torch.Tensor:
+
+    if mode == 'xyxy':
+        return box
+
+    box_out = box.clone()
+    if mode == 'xywh':
+        box_out[:, 2:] = box[:, 2:] - box[:, :2]
+    elif mode == 'cxcywh':
+        # nice little recursion
+        box_xywh = _bbox_xyxy_to_arbitrary(box, mode='xywh')
+        box_out[:, :2] = (box[:, :2] + box[:, 2:]) / 2
+        box_out[:, 2:] = box_xywh[:, 2:]
     else:
         raise NotImplementedError
 
--- pytorch_utils/test/label/test_bbox.py
+++ pytorch_utils/test/label/test_bbox.py
@@ -1,17 +1,46 @@
+from unittest import mock
 import pytest
 import torch
 
 from pytorch_utils.label import bbox
 
 
-@pytest.mark.parametrize("box_in,mode_in,mode_out,box_expct", [
-    (torch.Tensor([[1., 2., 3., 4.]]), 'xyxy', 'xyxy', torch.Tensor([[1., 2., 3., 4.]])),
-    (torch.Tensor([[1., 2., 3., 4.]]), 'xywh', 'xywh', torch.Tensor([[1., 2., 3., 4.]])),
-    (torch.Tensor([[1., 2., 3., 4.]]), 'xyxy', 'xywh', torch.Tensor([[1., 2., 2., 2.]])),
-    (torch.Tensor([[1., 2., 3., 4.]]), 'xywh', 'xyxy', torch.Tensor([[1., 2., 4, 6.]])),
+@pytest.mark.parametrize("mode_in, mode_out", [
+    ('xyxy', 'xyxy'),
+    ('xyxy', 'xywh'),
+    ('xywh', 'xyxy'),
+    ('xywh', 'xywh'),
 ])
-def test_convert_bbox(box_in, mode_in, mode_out, box_expct):
+def test_convert_bbox(mode_in, mode_out):
 
-    box_out = bbox.convert_bbox(box_in, mode_in, mode_out)
+    with mock.patch.object(bbox, '_bbox_arbitrary_to_xyxy') as arb_to_xyxy:
+        with mock.patch.object(bbox, '_bbox_xyxy_to_arbitrary') as xyxy_to_arb:
+            bbox.convert_bbox(torch.LongTensor([[1., 2., 3., 4.]]), mode_in=mode_in, mode_out=mode_out)
 
+    if mode_in == mode_out:
+        xyxy_to_arb.assert_not_called()
+        arb_to_xyxy.assert_not_called()
+    else:
+        xyxy_to_arb.assert_called()
+        arb_to_xyxy.assert_called()
+
+
+@pytest.mark.parametrize("box,mode,box_expct", [
+    (torch.Tensor([[1., 2., 3., 4.]]), 'xyxy', torch.Tensor([[1., 2., 3., 4.]])),
+    (torch.Tensor([[1., 2., 3., 4.]]), 'xywh', torch.Tensor([[1., 2., 4., 6.]])),
+])
+def test__bbox_arbitrary_to_xyxy(box, mode, box_expct):
+
+    box_out = bbox._bbox_arbitrary_to_xyxy(box, mode=mode)
+    assert (box_out == box_expct).all()
+
+
+@pytest.mark.parametrize("box,mode,box_expct", [
+    (torch.Tensor([[1., 2., 3., 4.]]), 'xyxy', torch.Tensor([[1., 2., 3., 4.]])),
+    (torch.Tensor([[1., 2., 3., 4.]]), 'xywh', torch.Tensor([[1., 2., 2., 2.]])),
+    (torch.Tensor([[1., 2., 3., 4.]]), 'cxcywh', torch.Tensor([[2., 3., 2., 2.]])),
+])
+def test__bbox_xyxy_to_arbitrary(box, mode, box_expct):
+
+    box_out = bbox._bbox_xyxy_to_arbitrary(box, mode=mode)
     assert (box_out == box_expct).all()
