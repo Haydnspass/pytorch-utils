24255af67e15416a23374c35a83f7cff2b201eb7
Lucas-Raphael Mueller <lucas-raphael.mueller@dkfz-heidelberg.de>
2021-05-20 15:59:06 +0200

    Fix non pickleable stuff

--- pytorch_utils/label/label.py
+++ pytorch_utils/label/label.py
@@ -8,6 +8,27 @@ import torch
 from torch.utils.data._utils.collate import default_collate
 
 
+class _LabelOnlyDS(torch.utils.data.Dataset):
+    """In order to be able to ignore the non label stuff which could confuse the temporary dataloader"""
+
+    def __init__(self, ds_core, label_ix, extract_fn):
+        super().__init__()
+        self.ds_core = ds_core
+        self.label_ix = label_ix
+        self.extract_fn = extract_fn
+
+    def __len__(self):
+        return self.ds_core.__len__()
+
+    def __getitem__(self, ix):
+        if self.extract_fn is not None:
+            return self.extract_fn(self.ds_core.__getitem__(ix))
+        elif self.label_ix is not None:
+            return self.ds_core.__getitem__(ix)[self.label_ix]
+        else:
+            return self.ds_core.__getitem__(ix)
+
+
 def get_all_labels(ds: torch.utils.data.Dataset, label_ix: int = 1,
                     extract_fn: Optional[Callable] = None,
                     batch_size=64, num_workers=4) -> torch.Tensor:
@@ -22,30 +43,12 @@ def get_all_labels(ds: torch.utils.data.Dataset, label_ix: int = 1,
         num_workers: number of workers for auxiliary dl
 
     """
-    class LabelOnlyDS(torch.utils.data.Dataset):
-        """In order to be able to ignore the non label stuff which could confuse the temporary dataloader"""
-        def __init__(self, ds_core, label_ix, extract_fn):
-            super().__init__()
-            self.ds_core = ds_core
-            self.label_ix = label_ix
-            self.extract_fn = extract_fn
-
-        def __len__(self):
-            return self.ds_core.__len__()
-
-        def __getitem__(self, ix):
-            if self.extract_fn is not None:
-                return self.extract_fn(self.ds_core.__getitem__(ix))
-            elif self.label_ix is not None:
-                return self.ds_core.__getitem__(ix)[self.label_ix]
-            else:
-                return self.ds_core.__getitem__(ix)
 
     if label_ix is not None and extract_fn is not None:
         warnings.warn("Ignoring label_ix because extract_fn was defined. Pass label_ix=None to avoid this warning.")
         label_ix = None
 
-    ds = LabelOnlyDS(ds_core=ds, label_ix=label_ix, extract_fn=extract_fn)
+    ds = _LabelOnlyDS(ds_core=ds, label_ix=label_ix, extract_fn=extract_fn)
     dl = torch.utils.data.DataLoader(ds, batch_size=batch_size, num_workers=num_workers, collate_fn=flexible_collate)
 
     label_cache = set()
--- test/test_label_label.py
+++ test/test_label_label.py
@@ -1,7 +1,3 @@
-import pytest
-from unittest import mock
-
-import sklearn.preprocessing
 import torch
 
 from pytorch_utils.label import label
@@ -17,45 +13,55 @@ def test_get_all_labels():
     assert (y == yout).all()
 
 
+class _LabelDSDict(torch.utils.data.Dataset):
+    def __len__(self):
+        return 100
+
+    def __getitem__(self, ix):
+        return torch.rand(3, 32, 32), {'objects': torch.arange(ix, ix + 3)}
+
+
+def _label_ds_dict_extract_fn(x):
+    return x[1]['objects']
+
+
 def test_get_all_labels_complex():
-    class AdvancedLabelDS(torch.utils.data.Dataset):
-        def __len__(self):
-            return 100
 
-        def __getitem__(self, ix):
-            return torch.rand(3, 32, 32), {'objects': torch.arange(ix, ix + 3)}
-
-    ds = AdvancedLabelDS()
-    y_out = label.get_all_labels(AdvancedLabelDS(), extract_fn=lambda x: x[1]['objects'])
+    ds = _LabelDSDict()
+    y_out = label.get_all_labels(_LabelDSDict(), extract_fn=_label_ds_dict_extract_fn)
 
     assert (y_out == torch.arange(102)).all()
 
 
+class _LabelDSUnequal(torch.utils.data.Dataset):
+    def __len__(self):
+        return 2
+
+    def __getitem__(self, ix):
+        if ix == 0:
+            return ['a']
+        else:
+            return ['a', 'b']
+
+
 def test_get_all_label_unequal():
-    class AdvancedLabelDS(torch.utils.data.Dataset):
-        def __len__(self):
-            return 2
 
-        def __getitem__(self, ix):
-            if ix == 0:
-                return ['a']
-            else:
-                return ['a', 'b']
-
-    y_out = label.get_all_labels(AdvancedLabelDS(), label_ix=None, num_workers=4)
+    y_out = label.get_all_labels(_LabelDSUnequal(), label_ix=None, num_workers=4)
 
     assert sorted(y_out) == ['a', 'b']
 
 
+class _LabelDSNonNumeric(torch.utils.data.Dataset):
+    def __len__(self):
+        return 100
+
+    def __getitem__(self, ix):
+        return torch.rand(3, 32, 32), ['bike', 'screen']
+
+
 def test_get_all_labels_nonnumeric():
-    class AdvancedLabelDS(torch.utils.data.Dataset):
-        def __len__(self):
-            return 100
 
-        def __getitem__(self, ix):
-            return torch.rand(3, 32, 32), ['bike', 'screen']
-
-    ds = AdvancedLabelDS()
-    y_out = label.get_all_labels(AdvancedLabelDS())
+    ds = _LabelDSNonNumeric()
+    y_out = label.get_all_labels(_LabelDSNonNumeric())
 
     assert y_out == {'bike', 'screen'}
