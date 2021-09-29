f73423120f5762c90341ab4f69d4d595d0abeb67
Lucas-Rapahel Müller <gitdev@lrm.photo>
2021-09-29 09:25:58 +0200

    Add hand graph to plotting utils

--- pytorch_utils/plot/__init__.py
+++ pytorch_utils/plot/__init__.py
@@ -1,3 +1,17 @@
 from . import plot
 
 from .plot import tshow, plot_bboxes, plot_keypoints
+
+
+def _make_hand_graph():
+    graph = []
+    graph = graph + [(k, k + 1) for k in range(4)]  # thumb
+    graph = graph + [(0, 5)] + [(k, k + 1) for k in range(5, 8)]  # 2
+    graph = graph + [(0, 9)] + [(k, k + 1) for k in range(9, 12)]  # 3
+    graph = graph + [(0, 13)] + [(k, k + 1) for k in range(13, 16)]  # 4
+    graph = graph + [(0, 17)] + [(k, k + 1) for k in range(17, 20)]  # 5
+
+    return graph
+
+
+hand_graph = _make_hand_graph()
