60ed8d623cd31d4d9fa726234a2fd168a7103655
Lucas-Rapahel Müller <gitdev@lrm.photo>
2021-11-22 10:52:39 +0100

    Bump version: 0.1.0dev1 → 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = []  # for now conda only
 
 setup(
     name="pytorch_utils",
-    version="0.1.0dev1",  # DO NOT MODIFY BY HAND; see DEVELOPER.md
+    version="0.1.0",  # DO NOT MODIFY BY HAND; see DEVELOPER.md
     packages=setuptools.find_packages(),
     install_requires=requirements,
     zip_safe=False,
