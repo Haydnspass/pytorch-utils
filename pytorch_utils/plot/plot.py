7e13270720cb41bfa066d41b59436371466abb51
Lucas-Raphael Mueller <gitdev@lrm.photo>
2021-12-21 17:11:00 +0100

    add cv2 backend for plotting

--- pytorch_utils/plot/plot.py
+++ pytorch_utils/plot/plot.py
@@ -1,5 +1,7 @@
 from typing import Optional, List
 
+import cv2
+import numpy as np
 import torch
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -9,8 +11,14 @@ from .. import lazy
 
 
 @lazy.cycle.torchify(0)
-def tshow(t: torch.Tensor, autosqueeze: bool = False, ax=None, *args, **kwargs):
-    """Tensor friendly plt.imshow"""
+def tshow(t: torch.Tensor, autosqueeze: bool = False, ax=None, im=None, backend: str = "matplotlib",
+          *args, **kwargs):
+    """
+    Tensor friendly plt.imshow
+
+    Args:
+        backend: matplotlib or cv2
+    """
 
     t = t.detach().cpu()
 
@@ -23,6 +31,15 @@ def tshow(t: torch.Tensor, autosqueeze: bool = False, ax=None, *args, **kwargs):
     if t.dim() == 3:
         t = t.permute(1, 2, 0)
 
+    if backend == "cv2":
+        t = t.numpy()
+        if t.ndim == 3:
+            t = cv2.cvtColor(t, cv2.COLOR_BGR2RGB)
+        return cv2.imshow("Figure", t)
+
+    if im is not None:
+        return im.set_data(t, *args, **kwargs)
+
     if ax is None:
         return plt.imshow(t, *args, **kwargs)
     else:
@@ -33,7 +50,9 @@ def tshow(t: torch.Tensor, autosqueeze: bool = False, ax=None, *args, **kwargs):
 @lazy.cycle.auto_device('cpu', 0)
 @lazy.tensor.view_to_dim_dec(2, 0, arg=0)
 def plot_bboxes(boxes: torch.Tensor, /, scores: Optional[torch.Tensor] = None, *,
-                box_mode='xyxy', order=None, ax=None, patch_kwargs=None, annotate_kwargs=None):
+                box_mode='xyxy', order=None, ax=None,
+                img: torch.Tensor = None, img_mode: str = "torch",
+                patch_kwargs=None, annotate_kwargs=None):
     """
     Plot bounding boxes on axis
 
@@ -57,36 +76,57 @@ def plot_bboxes(boxes: torch.Tensor, /, scores: Optional[torch.Tensor] = None, *
     if order == 'swapped':
         boxes = boxes[:, [1, 0, 3, 2]]
 
-    boxes_xywh = label.bbox.convert_bbox(boxes, box_mode, 'xywh')
+    if img is None:
+        boxes_xywh = label.bbox.convert_bbox(boxes, box_mode, 'xywh')
 
-    rectangles = [mpl.patches.Rectangle(b[[1, 0]], b[3], b[2], fill='', **patch_kwargs) for b in boxes_xywh]
+        rectangles = [mpl.patches.Rectangle(b[[1, 0]], b[3], b[2], fill='', **patch_kwargs) for b in boxes_xywh]
 
-    for i, r in enumerate(rectangles):
-        ax.add_patch(r)
+        for i, r in enumerate(rectangles):
+            ax.add_patch(r)
 
-        if scores is not None:
-            rx, ry = r.get_xy()
-            cx = rx
-            cy = ry + r.get_height()
+            if scores is not None:
+                rx, ry = r.get_xy()
+                cx = rx
+                cy = ry + r.get_height()
 
-            scores = scores.unsqueeze(0) if scores.dim() == 0 else scores
-            ax.annotate(f'p: {scores[i]:.2f}', (cx, cy), **annotate_kwargs)
+                scores = scores.unsqueeze(0) if scores.dim() == 0 else scores
+                ax.annotate(f'p: {scores[i]:.2f}', (cx, cy), **annotate_kwargs)
 
-    return ax
+        return ax
+    else:
+        img = torch_cv2(img) if img_mode == "torch" else img
+        color = [255, 0, 0] if "color" not in patch_kwargs else patch_kwargs["color"]
+        thickness_rect = 1 if "thickness" not in patch_kwargs else patch_kwargs["thickness"]
+
+        boxes = boxes[..., [1, 0, 3, 2]]
+        boxes = boxes.to(torch.int)
+        for b in boxes:
+            img = cv2.rectangle(img, b[:2].tolist(), b[2:].tolist(), color=color, thickness=thickness_rect)
+
+        img = cv2_torch(img) if img_mode == "torch" else img
+        return img
 
 
 @lazy.cycle.torchify(0)
 def plot_keypoints(keypoints: torch.Tensor, graph: Optional[List[tuple]] = None, plot_ix=True,
-                   ix_prefix: str = '', ax=None, plot_3d: bool = False):
+                   ix_prefix: str = '', ax=None, plot_3d: bool = False,
+                   img=None, img_mode="torch"):
     if ax is None:
         ax = plt.gca()
+
+    if img is not None and img_mode == "torch":
+        img = torch_cv2(img)
 
     keypoints = keypoints.detach().cpu()
 
     if graph is not None:
         for k, v in graph:
             if v is not None:
-                if not plot_3d:
+                if img is not None:
+                    pt0 = keypoints[[k, v]][0].to(torch.int).tolist()
+                    pt1 = keypoints[[k, v]][1].to(torch.int).tolist()
+                    img = cv2.line(img, pt0, pt1, color=[255, 0, 0], thickness=2)
+                elif not plot_3d:
                     ax.plot(keypoints[[k, v], 0], keypoints[[k, v], 1], color='blue')
                 else:
                     ax.plot(
@@ -95,19 +135,41 @@ def plot_keypoints(keypoints: torch.Tensor, graph: Optional[List[tuple]] = None,
                         keypoints[[k, v], 2],
                         color='blue')
 
-    if not plot_3d:
+    if img is not None:
+        for p in keypoints:
+            img = cv2.circle(img, p[:2].to(torch.int).tolist(),
+                             radius=3, color=[0, 0, 255], thickness=-1)
+    elif not plot_3d:
         ax.plot(keypoints[:, 0], keypoints[:, 1], 'ro')
     else:
         ax.plot(keypoints[:, 0], keypoints[:, 1], keypoints[:, 2], 'ro')
 
     if plot_ix:
         for i, kp in enumerate(keypoints):
-            if not plot_3d:
+            if img is not None:
+                pass
+            elif not plot_3d:
                 ax.text(kp[0], kp[1], ix_prefix + str(i))
             else:
                 ax.text(kp[0], kp[1], kp[2], ix_prefix + str(i))
 
-    return ax
+    if img is None:
+        return ax
+    if img_mode == "torch":
+        img = cv2_torch(img)
+    return img
+
+
+def torch_cv2(img: torch.Tensor) -> torch.Tensor:
+    if img.dim() == 3:
+        return img.permute(1, 2, 0)[..., [-1, 1, 0]].numpy()
+    return img.numpy()
+
+
+def cv2_torch(img: np.array) -> torch.Tensor:
+    if img.ndim == 3:
+        return torch.from_numpy(img).permute(-1, 0, 1)[[-1, 1, 0]]
+    return torch.from_numpy(img)
 
 
 class PlotKeypoints:
--- test/test_plot_plot.py
+++ test/test_plot_plot.py
@@ -1,34 +1,48 @@
+import tempfile
 import unittest.mock
-import matplotlib.pyplot as plt
-import pytest
 
+import cv2
+from matplotlib import cbook
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
 import torch
 
 import pytorch_utils.plot.plot
 
 
-def test_tshow():
+@pytest.fixture()
+def rgb_sample():
+    with cbook.get_sample_data('grace_hopper.jpg') as image_file:
+        return torch.from_numpy(plt.imread(image_file)).permute(-1, 0, 1)
+
+
+@pytest.mark.parametrize("backend", ["matplotlib", "cv2"])
+def test_tshow(backend):
     x = torch.rand(3, 30, 32)
     xn = x.permute(1, 2, 0).clone().numpy()
 
-    with unittest.mock.patch('matplotlib.pyplot.imshow') as mimshow:
-        pytorch_utils.plot.plot.tshow(x, cmap='gray')
+    if backend == "matplotlib":
+        with unittest.mock.patch('matplotlib.pyplot.imshow') as mimshow:
+            pytorch_utils.plot.plot.tshow(x, cmap='gray', backend=backend)
+    elif backend == "cv2":
+        with unittest.mock.patch("cv2.imshow") as mimshow:
+            pytorch_utils.plot.plot.tshow(x, backend=backend)
 
     mimshow.assert_called_once()
-    imshow_arg, = mimshow.call_args.args
+    imshow_args = mimshow.call_args.args
     imshow_kwargs = mimshow.call_args.kwargs
 
-    assert imshow_arg.size() == torch.Size([30, 32, 3])
-    assert 'cmap' in imshow_kwargs.keys()
+    if backend == "matplotlib":
+        assert imshow_args[0].size() == torch.Size([30, 32, 3])
+        assert 'cmap' in imshow_kwargs.keys()
+    if backend == "cv2":
+        assert imshow_args[1].shape == (30, 32, 3)
 
 
-@pytest.mark.graphic
-@pytest.mark.manual
-@pytest.mark.parametrize("plot_3d", [True, False])
-def test_plot_keypoints(plot_3d):
-    x = torch.rand(5, 3)
-
-    graph = [
+@pytest.fixture()
+def graph():
+    return [
         (0, 1),
         (1, 3),
         (2, 3),
@@ -36,23 +50,52 @@ def test_plot_keypoints(plot_3d):
         (5, None),
     ]
 
-    if plot_3d:
-        f = plt.figure()
-        ax = f.add_subplot(projection='3d')
-    else:
-        ax = None
 
-    pytorch_utils.plot.plot.plot_keypoints(x, graph, ax=ax, plot_3d=plot_3d)
+@pytest.mark.graphic
+@pytest.mark.manual
+@pytest.mark.parametrize("cv2_backend", [False, True])
+def test_plot_keypoints(cv2_backend, rgb_sample, graph):
+    x = torch.Tensor([[210, 180], [315, 170], [230, 250], [280, 200], [280, 310]])
+
+    if not cv2_backend:
+        pytorch_utils.plot.plot.plot_keypoints(x, graph)
+    else:
+        rgb_sample = pytorch_utils.plot.plot.plot_keypoints(x, graph, img=rgb_sample)
+    pytorch_utils.plot.plot.tshow(rgb_sample)
     plt.show()
 
 
 @pytest.mark.graphic
 @pytest.mark.manual
-def test_plot_bbox():
-    # 2D
-    pytorch_utils.plot.plot.plot_bboxes(torch.rand(2, 4))
+def test_plot_keypoints_3d(graph):
+    x = torch.rand(5, 3)
+
+    f = plt.figure()
+    ax = f.add_subplot(projection='3d')
+    pytorch_utils.plot.plot.plot_keypoints(x, graph, ax=ax, plot_3d=True)
     plt.show()
 
-     # 1D
-    pytorch_utils.plot.plot.plot_bboxes(torch.rand(4))
+
+@pytest.mark.graphic
+@pytest.mark.manual
+@pytest.mark.parametrize("cv2_backend", [False, True])
+def test_plot_bbox(cv2_backend, rgb_sample):
+    b = torch.Tensor([[50, 150, 400, 450], [180, 200, 220, 230]])
+
+    if not cv2_backend:
+        pytorch_utils.plot.plot.plot_bboxes(b)
+    else:
+        rgb_sample = pytorch_utils.plot.plot.plot_bboxes(b, img=rgb_sample)
+    pytorch_utils.plot.plot.tshow(rgb_sample)
     plt.show()
+
+
+@pytest.mark.parametrize("img", [torch.rand(3, 32, 64), torch.rand(32, 64)])
+def test_torch_cv2_conversion(img):
+
+    np.testing.assert_array_almost_equal(
+        pytorch_utils.plot.plot.cv2_torch(
+            pytorch_utils.plot.plot.torch_cv2(img)
+        ).numpy(),
+        img.numpy()
+    )
